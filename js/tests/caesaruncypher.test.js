e86d297fd81a33d7a77a7187fc7788acb4f2f169
firmo holanda <firmo@viize.com>
2020-06-05 17:50:27 -0300

    edit caesarsruncypher

--- js/tests/caesaruncypher.test.js
+++ js/tests/caesaruncypher.test.js
@@ -4,7 +4,7 @@ test('should uncypher something', () => {
   expect(caesar('uftu', 1)).toEqual('test');
 });
 test('should uncypher the alphabet', () => {
-  expect(caesar('bcdefghijklmnopqrstuvwxyza', 1)).toEqual('abcdefghijklmnopqrstuvwxyz');
+  expect(caesar('bcdefghijklmnopqrstuvwxyza', 1)).toEqual('abcdefghijklmnopqrstuvwxyF');
 });
 test('should not uncypher number', () => {
   expect(caesar('123', 1)).toEqual('123');
